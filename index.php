21b0906cec83b1ac026f1f9f38293f65948386c1
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2013-10-21 19:42:55 +0200

    improved caching to check headers

--- index.php
+++ index.php
@@ -7,12 +7,17 @@ $password = "bar";
 
 
 
+$headers = apache_request_headers();
 
+$newHeaders = array();
+foreach ($headers as $header => $value) {
+    $newHeaders[] = "$header: $value";
+}
 
-
+$lang = (isset($headers['Accept-Language'])) ? $headers['Accept-Language'] : 'de';
 
 $getPart = $_SERVER['REQUEST_URI'];
-$filename = dirname(__FILE__).'/cache/'.urlencode($getPart);
+$filename = dirname(__FILE__).'/cache/'.sha1($lang.'_'.urlencode($getPart));
 
 if (file_exists($filename)) {
     echo file_get_contents($filename);
@@ -29,8 +34,9 @@ $options = array(
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_USERPWD => $username . ":" . $password,
 );
-curl_setopt_array( $ch, $options );
 
+curl_setopt_array( $ch, $options );
+curl_setopt($ch,CURLOPT_HTTPHEADER, $newHeaders);
 $result =  curl_exec($ch);
 file_put_contents($filename,$result);
 
